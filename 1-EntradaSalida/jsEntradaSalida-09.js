b9f185518fb0f98b1ac2447521c2137238035b5d
Francisco Cantariño <cantafd@gmail.com>
2020-07-15 11:47:07 -0300

    ejercicio 9

--- 1-EntradaSalida/jsEntradaSalida-09.js
+++ 1-EntradaSalida/jsEntradaSalida-09.js
@@ -5,6 +5,19 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
+	var sueldo;
+	
 
-	alert("ok");
+	/*Se ingresan los numeros y se asignan al valor del identificador */
+	sueldo = prompt("Ingrese sueldo.");
+	txtIdSueldo.value = sueldo;
+
+	/*Se convierten en numero*/
+	sueldo = parseInt(sueldo, 10);
+	
+	/*Se realiza la operacion y se asigna el aumento*/
+	txtIdResultado.value = sueldo+(sueldo/10);
+
+	alert("Aumento realizado.");
+
 }
