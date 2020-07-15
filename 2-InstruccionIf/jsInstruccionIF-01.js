21eceb776ea0ca42b404cc5aad56b07a93a89be7
Francisco Cantariño <cantafd@gmail.com>
2020-07-15 17:32:57 -0300

    ejercicio 1 if

--- 2-InstruccionIf/jsInstruccionIF-01.js
+++ 2-InstruccionIf/jsInstruccionIF-01.js
@@ -1,6 +1,16 @@
 function mostrar()
 {
-	//tomo la edad  
-	alert("ok");
+	//tomo la edad
+	var edad;
+	edad = txtIdEdad.value;
+
+	//lo convierte en numero y lo revisa
+	edad = parseInt (edad, 10);
+	if (edad<15) {
+		alert("No puede subir.")
+	} else{
+		alert("Puede subir.")
+	}; 
+	
 
 }//FIN DE LA FUNCIÓN
