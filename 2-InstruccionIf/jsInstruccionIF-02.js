937e48e5befcdc3f453669ba7c8c129a938eba99
Francisco Cantariño <cantafd@gmail.com>
2020-07-15 17:38:59 -0300

    ejercicio 2 if

--- 2-InstruccionIf/jsInstruccionIF-02.js
+++ 2-InstruccionIf/jsInstruccionIF-02.js
@@ -1,7 +1,15 @@
 function mostrar()
 {
-	//tomo la edad  
-	alert("ok");
+	//tomo la edad
+	var edad;
+	edad = txtIdEdad.value;
+	
+	//lo convierto en numero
+	edad = parseInt(edad, 10);
+
+	//lo compara y avisa, si es mayor
+	if (edad>18){alert("Usted es mayor de edad.")};
+	
 	
 
 }//FIN DE LA FUNCIÓN
