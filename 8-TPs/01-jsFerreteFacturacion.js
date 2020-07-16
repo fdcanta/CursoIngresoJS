30ab1a742470aec3870dc0e09a9000de0f3433d3
Francisco Cantariño <cantafd@gmail.com>
2020-07-16 09:48:30 -0300

    Primer TP

--- 8-TPs/01-jsFerreteFacturacion.js
+++ 8-TPs/01-jsFerreteFacturacion.js
@@ -5,13 +5,59 @@ C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
 function Sumar () 
 {
-	
+    //Declaracion de variables
+    var precio1, precio2, precio3, total; 
+
+    //Toma los numeros y los convierte a decimal
+    precio1 = txtIdPrecioUno.value;
+    precio1 = parseInt(precio1, 10);
+    precio2 = txtIdPrecioDos.value;
+    precio2 = parseInt(precio2, 10);
+    precio3 = txtIdPrecioTres.value;
+    precio3 = parseInt(precio3, 10);
+
+    //Suma los numeros
+    total = (precio1+precio2+precio3);
+
+    //Imprime la suma
+    alert("El total bruto de los productos es "+total);
+    
 }
 function Promedio () 
 {
-	
+    //Declaracion de variables
+    var precio1, precio2, precio3, total; 
+
+    //Toma los numeros y los convierte a decimal
+    precio1 = txtIdPrecioUno.value;
+    precio1 = parseInt(precio1, 10);
+    precio2 = txtIdPrecioDos.value;
+    precio2 = parseInt(precio2, 10);
+    precio3 = txtIdPrecioTres.value;
+    precio3 = parseInt(precio3, 10);
+
+    //Saca el promedio
+    total = (precio1+precio2+precio3)/3;
+
+    //Imprime el promedio
+    alert("El promedio de su compra es "+total);
 }
 function PrecioFinal () 
 {
-	
+    //Declaracion de variables
+    var precio1, precio2, precio3, total; 
+
+    //Toma los numeros y los convierte a decimal
+    precio1 = txtIdPrecioUno.value;
+    precio1 = parseInt(precio1, 10);
+    precio2 = txtIdPrecioDos.value;
+    precio2 = parseInt(precio2, 10);
+    precio3 = txtIdPrecioTres.value;
+    precio3 = parseInt(precio3, 10);
+
+    //Calcula el total con iva
+    total = (precio1+precio2+precio3)*1.21;
+
+    //Imprime el resultado con iva
+    alert("El total neto de los productos es "+total)
 }
