4c34fe11791eb123c13aefa98cf8ada9c86df647
Francisco Cantariño <cantafd@gmail.com>
2020-07-16 10:38:31 -0300

    Segundo TP

--- 8-TPs/02-jsFerreteConstruccion.js
+++ 8-TPs/02-jsFerreteConstruccion.js
@@ -6,13 +6,61 @@ C.	Para hacer un contrapiso de 1m x 1m se necesitan 2 bolsas de cemento y 3 de c
 
 function Rectangulo () 
 {
+    //Se declaran las variables
+    var largo, ancho, alambre;
+
+    //se les asigna un valor
+    largo = txtIdLargo.value;
+    ancho = txtIdAncho.value;
+
+    //se pasan a numeros
+    largo = parseInt(largo, 10);
+    ancho = parseInt(ancho, 10);
+
+    //se calcula el perimetro y la cantidad de alambre
+    alambre = ((largo+ancho)*2)*3;
+
+    alert("se necesitan "+alambre+" metros de alambre.");
 
 }
 function Circulo () 
 {
+    //Se declaran las variables
+    var diametro, alambre;
+
+    //se calcula el perimetro
+    diametro = txtIdRadio.value*6.18;
+    
+
+    //se pasa a numero
+    diametro = parseInt(diametro, 10);
+
+    //se calcula la cantidad de alambre
+    alambre = diametro*3;
+
+    //se imprime cuanto alambre se necesita
+    alert("Se necesitan "+alambre+" metros de alambre.")
 	
 }
 function Materiales () 
 {
-	
+	//Se declaran las variables
+    var largo, ancho, materialArena, materialCal;
+
+    //se les asigna un valor
+    largo = txtIdLargo.value;
+    ancho = txtIdAncho.value;
+
+    //se pasan a numeros
+    largo = parseInt(largo, 10);
+    ancho = parseInt(ancho, 10);
+
+    //se calcula la superficie y los materiales
+    materialArena = (largo*ancho)*2;
+    materialCal= (largo*ancho)*3;
+
+    //se imprime la cantidad necesaria
+    alert("Se necesitan "+materialArena+" bolsas de arena, y "+materialCal+" bolsas de cal.")
+
+
 }
