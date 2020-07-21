54259ef298c68a5110f6769cdfe3ce2fda8af3b6
Francisco Cantariño <cantafd@gmail.com>
2020-07-21 11:41:27 -0300

    ejercicio 6 de if

--- 2-InstruccionIf/jsInstruccionIF-06.js
+++ 2-InstruccionIf/jsInstruccionIF-06.js
@@ -1,8 +1,24 @@
 function mostrar()
 {
-	//tomo la edad  
-	alert("ok");
+	//declara variables y toma el valor ingresado 
+	var edad;
+	edad = txtIdEdad.value;
+	
+	//lo convierte en numero
+	edad = parseInt(edad, 10);
+	
+	
 
+	//los revisa y avisa la franja de edad
+	if (edad>17){
+		alert("Usted es mayor de edad.");
+	} else {
+		if (edad<13){
+			alert("usted es menor de edad");
+		} else {
+			alert("Usted es adolescente");
+		}
+	}
 
 
 }//FIN DE LA FUNCIÓN
