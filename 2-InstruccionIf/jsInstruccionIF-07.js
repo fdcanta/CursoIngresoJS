dbd86d9da1255d14dd1e056cbc518a93e8924b95
Francisco Cantariño <cantafd@gmail.com>
2020-07-21 17:58:08 -0300

    ejercicio 7 y 8 con if

--- 2-InstruccionIf/jsInstruccionIF-07.js
+++ 2-InstruccionIf/jsInstruccionIF-07.js
@@ -1,8 +1,17 @@
 function mostrar()
 {
-	//tomo la edad  
-	alert("ok");
+	//declara variables y toma el valor ingresado 
+	var edad, estado;
+	edad = txtIdEdad.value;
+	estado = estadoCivil.value;
+
+	//lo convierte en numero
+	edad = parseInt(edad, 10);
+	
 	
 
-
+	//los revisa y avisa la franja de edad
+	if (edad<18 && estado=="Soltero") {alert("Es muy pequeño para NO ser soltero.")}
+	
+	
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF-08.js
+++ 2-InstruccionIf/jsInstruccionIF-08.js
@@ -1,8 +1,15 @@
 function mostrar()
 {
-	//tomo la edad  
-	alert("ok");
+	//declara variables y toma el valor ingresado 
+	var edad, estado;
+	edad = txtIdEdad.value;
+	estado = estadoCivil.value;
+
+	//lo convierte en numero
+	edad = parseInt(edad, 10);
+	
 	
 
-
+	//los revisa y avisa la franja de edad
+	if (edad>18 && estado=="Soltero") {alert("Es soltero y no es menor.")}
 }//FIN DE LA FUNCIÓN
