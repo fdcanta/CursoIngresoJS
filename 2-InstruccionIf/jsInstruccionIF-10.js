ce28f9acb360847ccc76699acb898b4935b7761e
Francisco Cantariño <cantafd@gmail.com>
2020-07-22 09:21:08 -0300

    ejercicio 10 random

--- 2-InstruccionIf/jsInstruccionIF-10.js
+++ 2-InstruccionIf/jsInstruccionIF-10.js
@@ -1,6 +1,21 @@
 function mostrar()
 {
+	//se crean las variables
+	var num1;
+	
 	//Genero el número RANDOM entre 1 y 10 
-	alert("ok");
+	num1 = Math.ceil(Math.random(10) * 10)
+	
 
+	//los revisa y avisa la franja de num1
+	if (num1>8){
+		alert("su nota es "+num1+", excelente!");
+	} else {
+	if (num1>4){
+		alert("su nota es "+num1+", aprobó.");
+	} else {
+		alert("su nota es "+num1+". La proxima será.");
+	}
+	}
+	
 }//FIN DE LA FUNCIÓN
