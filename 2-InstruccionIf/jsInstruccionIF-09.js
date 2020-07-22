4e865092bbf12aca9b35da50bd46a484305498c0
Francisco Cantariño <cantafd@gmail.com>
2020-07-22 10:36:37 -0300

    ejercicio 9 y 10 aleatorio, con metodo dinamico

--- 2-InstruccionIf/jsInstruccionIF-09.js
+++ 2-InstruccionIf/jsInstruccionIF-09.js
@@ -2,9 +2,17 @@ function mostrar()
 {
 	//se crean las variables
 	var num1;
+	var max;
+	var min;
+
+	//se asignan las variables
+	var max = 11;
+	var min = 1;
 	
-	//Genero el número RANDOM entre 1 y 10 
-	num1 = Math.ceil(Math.random(10) * 10)
-	alert("El numero es "+num1);	
+	//Genero el número aleatorio entre 1 y 10 
+	num1 = min + Math.floor((max - min) * Math.random())
+
+	//lo muestra en consola
+	console.log("El numero es "+num1);	
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF-10.js
+++ 2-InstruccionIf/jsInstruccionIF-10.js
@@ -2,19 +2,24 @@ function mostrar()
 {
 	//se crean las variables
 	var num1;
+	var max;
+	var min;
+
+	//se asignan las variables
+	var max = 11;
+	var min = 1;
 	
-	//Genero el número RANDOM entre 1 y 10 
-	num1 = Math.ceil(Math.random(10) * 10)
-	
+	//Genero el número aleatorio entre 1 y 10 
+	num1 = min + Math.floor((max - min) * Math.random())
 
 	//los revisa y avisa la franja de num1
 	if (num1>8){
-		alert("su nota es "+num1+", excelente!");
+		console.log("su nota es "+num1+", excelente!");
 	} else {
 	if (num1>4){
-		alert("su nota es "+num1+", aprobó.");
+		console.log("su nota es "+num1+", aprobó.");
 	} else {
-		alert("su nota es "+num1+". La proxima será.");
+		console.log("su nota es "+num1+". La proxima será.");
 	}
 	}
 	
